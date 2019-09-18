027d16b3dfb13ca6587a3b4c11aa53184af8dfb0
sbm <ommaatoug@gmail.com>
2019-09-18 15:33:13 +0100

    add readme file

--- www/SbmCordovaPluginEpos2.js
+++ www/SbmCordovaPluginEpos2.js
@@ -12,10 +12,6 @@ module.exports.printText = function (arg0, success, error) {
     exec(success, error, 'SbmCordovaPluginEpos2', 'printText', [arg0]);
 };
 
-module.exports.discoverPrinters = function (arg0, success, error) {
-    exec(success, error, 'SbmCordovaPluginEpos2', 'discoverPrinters', [arg0]);
-};
-
 
 module.exports.stopDiscoverPrinters = function (arg0, success, error) {
     exec(success, error, 'SbmCordovaPluginEpos2', 'stopDiscoverPrinters', [arg0]);
